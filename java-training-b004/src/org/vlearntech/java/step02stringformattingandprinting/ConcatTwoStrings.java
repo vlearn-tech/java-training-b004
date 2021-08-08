31dbc2bfee20fddca2ddef0d72d0a40a1fd24ed2
vlearn-tech <vlearn.tech.official@gmail.com>
2021-08-08 09:35:42 +0530

    String concatenation

---
+++ java-training-b004/src/org/vlearntech/java/step02stringformattingandprinting/ConcatTwoStrings.java
@@ -0,0 +1,16 @@
+package org.vlearntech.java.step02stringformattingandprinting;
+
+public class ConcatTwoStrings {
+	public static void main(String[] args) {
+
+		String message = "Hello";
+//		message.concat("World");
+
+		message = "Hello" + " World";
+
+		System.out.println(message);
+		System.out.println(message);
+		System.out.println(message);
+
+	}
+}
