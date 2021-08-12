16c9c7d56efa177955c0190a98b3afa573038dd4
vlearn-tech <vlearn.tech.official@gmail.com>
2021-08-12 09:30:22 +0530

    Variables and Naming Conventions

---
+++ java-training-b004/src/org/vlearntech/java/step03variablesanddatatypes/VariablesAndNamingConventions.java
@@ -0,0 +1,28 @@
+package org.vlearntech.java.step03variablesanddatatypes;
+
+public class VariablesAndNamingConventions {
+	public static void main(String[] args) {
+
+		int age = 34;
+		boolean isMarried = true;
+		double lastDrawnSalary = 12345.5;
+		char jobLevel = 'X';
+
+		// Cases and naming conventions
+
+		// camel case: start with lower case, and then every word - first letter capital case
+		// lastDrawnSalary
+
+		// pascal case: same as camel case, but first letter is also capital
+		// LastDrawnSalary
+
+		// snake case: all character are small. but words are separated by _
+		// last_drawn_salary
+
+		// lower case - used for packages
+
+		// upper case - used for constants (we also use _ along with upper case)
+		// DEFAULT_EXPERIENCE = 0;
+
+	}
+}
