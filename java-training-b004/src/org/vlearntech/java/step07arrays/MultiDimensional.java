c9c68549a431e7f6fed00e4600546943f264d5e8
vlearn-tech <vlearn.tech.official@gmail.com>
2021-10-02 10:02:12 +0530

    How to declare a MDA.

---
+++ java-training-b004/src/org/vlearntech/java/step07arrays/MultiDimensional.java
@@ -0,0 +1,14 @@
+package org.vlearntech.java.step07arrays;
+
+public class MultiDimensional {
+	public static void main(String[] args) {
+		int[] numbers = { 1, 2, 3, 4, 5 };
+		int[][] scores = new int[8][6];
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; i < 6; j++) {
+//				scores[i][j] = 
+			}
+		}
+
+	}
+}
